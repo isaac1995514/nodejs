8de0e874d67d875ccb446dab6220e63d32d1e09c
Leong <isaacleong@TYCM15911-A.local>
2020-09-24 16:31:44 -0400

    Express Server Setup

--- production/src/index.js
+++ production/src/index.js
@@ -1,3 +1,16 @@
-import { sayHello } from "./helper";
+import express from "express";
 
-sayHello();
+// Create app instance
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+const server = app.listen(3030, () => console.log("Server Ready"));
+
+process.on("SiGTERM", () => {
+  server.close(() => {
+    console.log("Process terminated");
+  });
+});
